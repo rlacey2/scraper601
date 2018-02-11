202228d233885324921e59d9262fd520ec92e276
rlacey2@gmail.com <rlacey2@gmail.com>
2018-02-11 12:12:45 +0000

    fixes

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.use (function (req, res, next) {  // req.protocol
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header("Access-Control-Allow-Methods", "GET");  
+ // res.header("Access-Control-Allow-Methods", "GET");  
   next();
 });
 
